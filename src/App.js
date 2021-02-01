66e6d4d7a0fc74a4d3e508b7528637d4aadc40e8
farahalomar <53612293+farahalomar@users.noreply.github.com>
2021-02-01 15:04:21 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1 style={{textAlign: "center"}}>Cookies and Beyond</h1>
+     <h4 style={{textAlign: "center"}}>Where cookie maniacs gather</h4>
+     <img style={{
+  display: "block",
+  marginLeft: "auto",
+  marginRight: "auto",
+  width: "50%"
+}}
+        alt="cookie shop"
+        src="https://i.pinimg.com/originals/8f/cf/71/8fcf719bce331fe39d7e31ebf07349f3.jpg"
+    ></img>
     </div>
   );
 }
